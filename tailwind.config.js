4d9ddb78184f4f8c91fe749c5dd81a9f6d33f875
trevv16 <tnjeru16@gmail.com>
2021-06-08 18:49:22 -0400

    silenced eslint global-require error

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')]
+  // eslint-disable-next-line global-require
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')],
 };

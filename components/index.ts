7fed14590b2f93c9330aed550b70ea99ed973462
trevv16 <tnjeru16@gmail.com>
2021-06-08 14:08:50 -0400

    created a export file for components dir

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+// Layout
+export { default as Navigation } from "./layout/Navigation";
+export { default as Footer } from "./layout/Footer";
+export { default as Layout } from "./layout/Layout";
+
+// Utils
+export { default as SeoMeta } from "./SeoMeta";
+// export { default as Alert } from './Alert';
+export { default as Loading } from "./Loading";
+export { default as Icon } from "./Icon";

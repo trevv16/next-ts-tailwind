c85eb5374ae8352ed0a9139a134f7c7e8bb2aa73
trevv16 <tnjeru16@gmail.com>
2021-06-08 18:11:30 -0400

    updated SeoMeta comp

--- components/SeoMeta.tsx
+++ components/SeoMeta.tsx
@@ -1,17 +1,29 @@
 import Head from 'next/head';
 import { SITE_DESCRIPTION, SITE_IMG, SITE_IMG_ALT, SITE_LIVE_URL, SITE_NAME } from '../Config';
 
-export default function SeoMeta({ title, description, image, image_alt }) {
+type SeoMetaProps = {
+  title?: string;
+  description?: string;
+  image?: string;
+  image_alt?: string;
+};
+
+export default function SeoMeta({ title, description, image, image_alt }: SeoMetaProps) {
+  const siteName = title ? `${title} | ${SITE_NAME}` : SITE_NAME;
+  const siteDescription = description ? description : SITE_DESCRIPTION;
+  const siteImage = image ? image : SITE_IMG;
+  const siteImageAlt = image_alt ? image_alt : SITE_IMG_ALT;
+
   return (
     <Head>
-      <title>{`${title} | ${SITE_NAME}` || SITE_NAME}</title>
-      <meta name='description' content={description || SITE_DESCRIPTION} />
+      <title>{siteName}</title>
+      <meta name='description' content={siteDescription} />
 
       <meta property='og:type' content='website' />
-      <meta name='og:title' property='og:title' content={title || SITE_NAME} />
-      <meta name='og:description' property='og:description' content={description || SITE_DESCRIPTION} />
-      <meta property='og:site_name' content={SITE_NAME} />
-      <meta name='og:image' content={image || SITE_IMG} />
+      <meta name='og:title' property='og:title' content={siteName} />
+      <meta name='og:description' property='og:description' content={siteDescription} />
+      <meta property='og:site_name' content={siteName} />
+      <meta name='og:image' content={siteImage} />
 
       <link rel='canonical' href={SITE_LIVE_URL} />
 
@@ -19,12 +31,12 @@ export default function SeoMeta({ title, description, image, image_alt }) {
       <link rel='apple-touch-icon' href='/public/favicon.ico' />
 
       <meta name='twitter:card' content='summary' />
-      <meta name='twitter:title' content={title || SITE_NAME} />
-      <meta name='twitter:description' content={description || SITE_DESCRIPTION} />
-      <meta name='twitter:image' content={image || SITE_IMG} />
+      <meta name='twitter:title' content={siteName} />
+      <meta name='twitter:description' content={siteDescription} />
+      <meta name='twitter:image' content={siteImage} />
       <meta name='twitter:site' content='@trevva_' />
       <meta name='twitter:creator' content='@trevva_' />
-      <meta name='twitter:image:alt' content={image_alt || SITE_IMG_ALT} />
+      <meta name='twitter:image:alt' content={siteImageAlt} />
     </Head>
   );
 }
